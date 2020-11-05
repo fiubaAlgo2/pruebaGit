30b3dde3422535a65b141c195094d22cdeb43022
crpistillo <caropistillo@gmail.com>
2020-11-05 13:53:54 -0300

    fix

--- main.cpp
+++ main.cpp
@@ -7,13 +7,11 @@
 using namespace std;
 
 int main(){
-<<<<<<< HEAD
+
     cout << "Hola" << endl;
-=======
     int c = 344345;
     int b = 3;
     int a = 5;
     cout << a << endl;
     cout << "Hello world" << endl;
->>>>>>> main
 }
